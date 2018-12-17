c7f507fcd08ac7ff1df2fdc73a7551c21dad4fe4
giiiita <giiiita@giiiitanomacbook-puro.local>
2018-12-18 00:36:32 +0900

    Initial Commit

---
+++ FirebaseLoginTests/FirebaseLoginTests.swift
@@ -0,0 +1,34 @@
+//
+//  FirebaseLoginTests.swift
+//  FirebaseLoginTests
+//
+//  Created by giiiita on 2018/12/18.
+//  Copyright © 2018年 giiiita. All rights reserved.
+//
+
+import XCTest
+@testable import FirebaseLogin
+
+class FirebaseLoginTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
